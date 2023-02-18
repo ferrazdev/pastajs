8ced9ed9060667e769f30e4c84b788a688e195ce
Iratan Ferraz <ferraz34426@gmail.com>
2023-02-17 21:51:47 -0300

    modificação

---
+++ Aula 01 JS/script.js
@@ -0,0 +1,12 @@
+{
+    let primeironúmero = 10;
+    let segundonúmero = 5;
+    
+    console.log(primeironúmero + segundonúmero)
+}
+
+{
+    prompt()
+
+
+}
---
+++ Aula 02/script.js
@@ -0,0 +1,13 @@
+let primeiroNumero = 10;
+let segundoNumerlet =
+let Soma = 0;
+
+primeiroNumero = 10;
+segundoNumero = 5;
+
+primeiroNumero += 10
+segundoNumero += 10
+
+Soma = primeiroNumero + segundoNumero
+console.log(`Soma` , Soma) 
+console.log("Tipo de variavel numero", typeof(primeiroNumero))
